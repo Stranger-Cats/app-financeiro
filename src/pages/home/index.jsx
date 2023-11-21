230f1f6d62f749c13e15b940f79b33374c8383aa
Douglas Antunes da Silva <59983373+Douglastark123@users.noreply.github.com>
2023-11-21 19:33:59 -0400

    icon fix + push notification

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,63 @@
-import React from "react";
-import { View, Text } from "react-native";
+// import React from "react";
+// import { View, Text } from "react-native";
 
-export default function Home() {
-    return (
-        <View>
-            <Text>Tela Home</Text>
-        </View>
-    );
+// export default function Home() {
+//     return (
+//         <View>
+//             <Text>Tela Home</Text>
+//         </View>
+//     );
+// }
+
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { registerForPushNotificationsAsync, schedulePushNotification } from '../../services/PushNotificationService';
+
+
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
+  );
 }
